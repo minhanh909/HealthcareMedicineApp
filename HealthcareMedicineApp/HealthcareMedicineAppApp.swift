daa3df27b5667afa2d38e7b1d3b7238303847c2a
Minhanh Vo <minhanhvo225@gmail.com>
2026-03-15 15:49:57 +0700

    Initial Commit

---
+++ HealthcareMedicineApp/HealthcareMedicineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthcareMedicineAppApp.swift
+//  HealthcareMedicineApp
+//
+//  Created by Minh Anh on 15/3/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthcareMedicineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthcareMedicineAppTests/HealthcareMedicineAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthcareMedicineAppTests.swift
+//  HealthcareMedicineAppTests
+//
+//  Created by Minh Anh on 15/3/26.
+//
+
+import Testing
+@testable import HealthcareMedicineApp
+
+struct HealthcareMedicineAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
